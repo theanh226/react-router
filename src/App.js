6f50ff111170b6f7b040e0e2fd17461401c6862a
theanh <theanh@dev2.edunet24.de>
2018-09-03 18:16:10 +0200

    use custom link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+  return (
+    <Route
+      path={to}
+      exact={activeOnlyWhenExact}
+      children={({ match }) => {
+        var defaultStyleLink = "mr-2 text-danger";
+        var active = match ? "active mr-2" : defaultStyleLink;
+        return (
+          <li >
+            <Link  to={to} className={active} >
+              {label}
+            </Link>
+          </li>
+        );
+      }}
+    />
+  );
+};
 class App extends Component {
   render() {
     return (
@@ -11,33 +35,8 @@ class App extends Component {
         <div className="App">
           <nav className="navbar navbar-dark bg-dark">
             <ul className="nav navbar-nav d-flex flex-row w-75">
-              <li className="active cursor-pointer text-white mr-2 ">
-                <NavLink
-                  exact
-                  to="/"
-                  activeClassName="active"
-                  //or you can use this method to apparent link with key word 'activeStyle' inner style
-                  // activeStyle={{
-                  //   fontWeight: "bold",
-                  //   color: "red"
-                  // }}
-                  // className="text-white "
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="cursor-pointer text-white">
-                <NavLink
-                  to="/about"
-                  activeClassName="active"
-                  // activeStyle={{
-                  //   fontWeight: "bold",
-                  //   color: "red"
-                  // }}
-                >
-                  About
-                </NavLink>
-              </li>
+              <MenuLink label="Hompage" to="/" activeOnlyWhenExact={true} />
+              <MenuLink label="About" to="/about" />
             </ul>
           </nav>
           {/* content */}
