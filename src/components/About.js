ecd957b0ce78c5656500cef86927883b73cefcfe
theanh <theanh@dev2.edunet24.de>
2018-09-04 19:06:40 +0200

    Object Promt

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCheck: false
+    };
+  }
+
+  StateIsCheck = isCheck => {
+    this.setState({
+      isCheck: isCheck
+    });
+    console.log(this.state.isCheck);
+  };
+
   render() {
+    var { isCheck } = this.state;
     return (
       <div className="Container">
-        <h2 className="text-warning">This is About</h2>
+        <h2 className="text-warning">About Page</h2>
+        <button
+          className="btn btn-success"
+          onClick={() => this.StateIsCheck(false)}
+        >
+          Allowed
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => this.StateIsCheck(true)}
+        >
+          nicht allowed
+        </button>
+        <Prompt
+          when={isCheck}
+          message={location => `are you sure go to  ${location.pathname}`}
+        />
       </div>
     );
   }
